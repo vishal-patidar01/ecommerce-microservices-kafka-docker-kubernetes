936fe4e3a4789b1cd9d71f9c17b9e83de7ee4d2e
vishal patidar <vishalsuman8770@gmail.com>
2026-01-07 19:11:38 +0530

    Add inventory entity

---
+++ inventory-service/src/main/java/com/vishal/ecommerce/inventory_service/entity/Product.java
@@ -0,0 +1,27 @@
+package com.vishal.ecommerce.inventory_service.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Double price;
+
+    private Integer stock;
+}
