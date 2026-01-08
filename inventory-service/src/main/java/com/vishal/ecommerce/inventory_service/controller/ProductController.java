f8b0130be3a584a29163f91522af06f5162e0f99
vishal patidar <vishalsuman8770@gmail.com>
2026-01-09 02:30:21 +0530

    feat: update inventory and order controllers for API Gateway

--- inventory-service/src/main/java/com/vishal/ecommerce/inventory_service/controller/ProductController.java
+++ inventory-service/src/main/java/com/vishal/ecommerce/inventory_service/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.vishal.ecommerce.inventory_service.controller;
 
 import com.vishal.ecommerce.inventory_service.dto.ProductDto;
 import com.vishal.ecommerce.inventory_service.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
@@ -23,11 +24,14 @@ public class ProductController {
     private final RestClient restClient;
 
     @GetMapping("/fetchOrders")
-    public String fetchFromOrderService() {
+    public String fetchFromOrderService(HttpServletRequest httpServletRequest) {
+
+        log.info(httpServletRequest.getHeader("x-custom-header"));
+
         ServiceInstance orderService = discoveryClient.getInstances("order-service").getFirst();
 
         return restClient.get()
-                .uri(orderService.getUri()+"/api/v1/orders/helloOrders")
+                .uri(orderService.getUri()+"/orders/core/helloOrders")
                 .retrieve()
                 .body(String.class);
     }
